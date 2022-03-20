7b25c52e69bd3924662d53830cb494c31c53f1b4
Javier <javier96rn@gmail.com>
2022-03-20 20:05:45 +0100

    Create function to play single rounds

--- app.js
+++ app.js
@@ -8,3 +8,37 @@ function computerPlay() {
         return 'Scissors';
     }
 }
+
+function playRound(playerSelection, computerSelection) {
+    playerSelection = playerSelection.toLowerCase();
+    computerSelection = computerSelection.toLowerCase();
+
+    if (playerSelection === 'rock' && computerSelection === 'rock') {
+        return "It's a tie!";
+    } else if (playerSelection === 'rock' && computerSelection === 'paper') {
+        return 'You lose!';
+    } else if (playerSelection === 'rock' && computerSelection === 'scissors') {
+        return 'You win!';
+    } else if (playerSelection === 'paper' && computerSelection === 'rock') {
+        return 'You win!';
+    } else if (playerSelection === 'paper' && computerSelection === 'paper') {
+        return "It's a tie!";
+    } else if (
+        playerSelection === 'paper' &&
+        computerSelection === 'scissors'
+    ) {
+        return 'You lose!';
+    } else if (playerSelection === 'scissors' && computerSelection === 'rock') {
+        return 'You lose!';
+    } else if (
+        playerSelection === 'scissors' &&
+        computerSelection === 'paper'
+    ) {
+        return 'You win!';
+    } else if (
+        playerSelection === 'scissors' &&
+        computerSelection === 'scissors'
+    ) {
+        return "It's a tie!";
+    }
+}
